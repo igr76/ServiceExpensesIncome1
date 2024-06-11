4a31e0db22205f92a0c8630f339eb371f02104f9
igr76 <gritsuk766@gmail.com>
2024-06-11 23:02:05 +1000

    start

---
+++ src/main/java/com/example/serviceexpensesincome1/ServiceExpensesIncome1Application.java
@@ -0,0 +1,13 @@
+package com.example.serviceexpensesincome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExpensesIncome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceExpensesIncome1Application.class, args);
+	}
+
+}
